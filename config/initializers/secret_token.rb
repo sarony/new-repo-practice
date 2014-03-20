ef861b54c2724a4a34d8f0e29b3d8d72429b5724
sarony <saron.yitbarek@gmail.com>
2014-03-20 13:16:51 -0400

    fist git commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Speakerdeck::Application.config.secret_key_base = '9575dfc72a5f593fea2568683d1b97fd1388d527408f40208644b19ff9bec6bed7105e35676ccdc5e72d7c1edca351a938da689e225a4104bd461ee91ea7c2b7'
